3ec7d714a9583b592341735f5a502958f720beb4
[mid9coward] <[hieuvu0422@gmail.com]>
2025-02-12 19:56:40 +0700

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import { FilterProvider } from "./context/filter_context.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
-    clientId={import.meta.env.VITE_AUTH0_ID}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: window.location.origin,
+      redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
     }}
   >
     <UserProvider>
