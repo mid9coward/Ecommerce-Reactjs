689672ed7466ae0b99501fad5e46fce7a8466ed0
[mid9coward] <[hieuvu0422@gmail.com]>
2025-02-19 13:20:39 +0700

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -13,9 +13,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
     clientId={import.meta.env.VITE_AUTH0_ID}
     authorizationParams={{
-      redirect_uri: `${window.location.origin}/callback`,
+      redirect_uri: window.location.origin,
       audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-      scope: "openid profile email read:users",
     }}
   >
     <UserProvider>
