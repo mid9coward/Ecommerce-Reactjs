c0f60df133ebcfbcc3e3be019d91c4d4f58d619a
[mid9coward] <[hieuvu0422@gmail.com]>
2025-02-19 12:41:21 +0700

    Auth0 callback handle

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,11 +1,18 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
+import { Navigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useAuth0();
-  if (!user) {
-    return <Navigate to="/" />;
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        Loading...
+      </div>
+    );
   }
-  return children;
+
+  return isAuthenticated ? children : <Navigate to="/" />;
 };
+
 export default PrivateRoute;
