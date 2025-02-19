713a9111d66f4c7a1da31af84294f0128e7214e9
[mid9coward] <[hieuvu0422@gmail.com]>
2025-02-19 12:56:11 +0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   HomePage,
@@ -13,53 +11,20 @@ import {
 import { Footer, NavBar } from "./components";
 
 function App() {
-  const { isLoading, error, isAuthenticated, handleRedirectCallback } =
-    useAuth0();
-
-  // Xử lý Redirect Callback khi Auth0 trả về mã token
-  useEffect(() => {
-    const handleAuthRedirect = async () => {
-      try {
-        await handleRedirectCallback();
-      } catch (err) {
-        console.error("Auth0 Callback Error:", err);
-      }
-    };
-
-    if (
-      window.location.search.includes("code=") &&
-      window.location.search.includes("state=")
-    ) {
-      handleAuthRedirect();
-    }
-  }, []);
-
-  // Nếu đang tải Auth0, hiển thị màn hình loading
-  if (isLoading)
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        Loading...
-      </div>
-    );
-
-  // Nếu có lỗi, hiển thị thông báo lỗi
-  if (error)
-    return (
-      <div className="text-red-500 text-center mt-10">
-        Error: {error.message}
-      </div>
-    );
-
   return (
     <div className="relative min-h-screen">
       <Router>
         <NavBar />
         <main className="min-h-screen">
           <Routes>
-            <Route exact path="/" element={<HomePage />} />
-            <Route exact path="/cart" element={<CartPage />} />
-            <Route exact path="/products" element={<ProductsPage />} />
-            <Route exact path="/products/:id" element={<SingleProductPage />} />
+            <Route exact path="/" element={<HomePage />}></Route>
+            <Route exact path="/cart" element={<CartPage />}></Route>
+            <Route exact path="/products" element={<ProductsPage />}></Route>
+            <Route
+              exact
+              path="/products/:id"
+              element={<SingleProductPage />}
+            ></Route>
             <Route
               path="checkout"
               element={
--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -2,17 +2,10 @@ import { Navigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const PrivateRoute = ({ children }) => {
-  const { isAuthenticated, isLoading } = useAuth0();
-
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        Loading...
-      </div>
-    );
+  const { user } = useAuth0();
+  if (!user) {
+    return <Navigate to="/" />;
   }
-
-  return isAuthenticated ? children : <Navigate to="/" />;
+  return children;
 };
-
 export default PrivateRoute;
