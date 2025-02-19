20e92dbf7c1fddae3025ce3d8df9847de03ab28d
[mid9coward] <[hieuvu0422@gmail.com]>
2025-02-19 10:20:40 +0700

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,15 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { ProductsProvider } from "./context/products_context.jsx";
 import { CartProvider } from "./context/cart_context.jsx";
 import { FilterProvider } from "./context/filter_context.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
     clientId={import.meta.env.VITE_AUTH0_ID}
     authorizationParams={{
       redirect_uri: window.location.origin,
+      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+      scope: "openid profile email",
     }}
   >
     <UserProvider>
